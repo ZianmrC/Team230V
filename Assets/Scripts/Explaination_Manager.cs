f676457843f6fb0d5b31ae4c660c82875114ab2f
Kurruta <131220187+Kurruta@users.noreply.github.com>
2023-10-15 16:08:25 +1000

    Finished Tutorial + polished

--- Assets/Scripts/Explaination_Manager.cs
+++ Assets/Scripts/Explaination_Manager.cs
@@ -10,6 +10,11 @@ public class Explaination_Manager : MonoBehaviour
     public TextMeshProUGUI text;
     public GameObject Hazard;
     public GameObject Hazard2;
+    public GameObject ArrowArrow;
+    public GameObject ArrowScore;
+    public GameObject ArrowCounter;
+    public GameObject ArrowLife;
+    public GameObject ArrowParent;
     public string sceneToSwitchTo;
     private int counter = 0;
 
@@ -23,6 +28,11 @@ public class Explaination_Manager : MonoBehaviour
         ArrowExpl.SetActive(true);
         Hazard.SetActive(false);
         Hazard2.SetActive(false);
+        ArrowArrow.SetActive(false);
+        ArrowScore.SetActive(false);
+        ArrowCounter.SetActive(false);
+        ArrowLife.SetActive(false);
+        ArrowParent.SetActive(false);
         counter = 0;
         text.text = "Hey there! I’m Todd, a fellow safety hero!";
     }
@@ -44,7 +54,7 @@ public class Explaination_Manager : MonoBehaviour
             }
             else if (counter == 2)
             {
-                text.text = "First of all, we need to look for electrical hazards, " +
+                text.text = "First of all, we need to look for electrical problems, " +
                     "<br>like this one!";
                 Hazard.SetActive(true);
                 Hazard2.SetActive(true);
@@ -52,29 +62,49 @@ public class Explaination_Manager : MonoBehaviour
             }
             else if (counter == 3)
             {
-                text.text = "Solving them will give you safety points, and also stops the " +
-                    "<br>electricity from overloading!";
+                text.text = "They spawn randomly in the house, so you will need to <br> move around using the arrows on the left and right!";
                 Hazard.SetActive(false);
                 Hazard2.SetActive(false);
+                ArrowArrow.SetActive(true);
                 counter++;
             }
             else if (counter == 4)
             {
-                text.text = "The counter on the top shows how many hazards are present " +
-                    "<br>Throughout the house!";
+                text.text = "Solving them will give you safety points, and also stops the " +
+                    "<br>electricity from overloading!";
+                ArrowArrow.SetActive(false);
+                ArrowScore.SetActive(true);
                 counter++;
             }
             else if (counter == 5)
             {
-                text.text = "And finally, some hazards are very dangerous, so make sure to call <br>a parent by dragging them on it! ";
+                text.text = "The counter on the top shows how many hazards are present " +
+                    "<br>Throughout the house!";
+                ArrowScore.SetActive(false);
+                ArrowCounter.SetActive(true);
                 counter++;
             }
             else if (counter == 6)
             {
-                text.text = "That’s all from me, let’s go and stop that ghost!";
+                text.text = "And finally, some hazards are very dangerous, so make sure to call <br>a parent by dragging them on it! ";
                 counter++;
+                ArrowCounter.SetActive(false);
+                ArrowParent.SetActive(true);
             }
             else if (counter == 7)
+            {
+                text.text = "Make sure not to touch them yourself though! You might get badly hurt!";
+                counter++;
+                ArrowParent.SetActive(false);
+                ArrowLife.SetActive(true);
+            }
+            else if (counter == 8)
+            {
+                text.text = "That’s all from me, let’s go and stop that ghost!";
+                ArrowLife.SetActive(false);
+                counter++;
+            }
+            else if (counter == 9)
             {
                 ArrowExpl.SetActive(false);
                 SceneManager.LoadSceneAsync(sceneToSwitchTo);
