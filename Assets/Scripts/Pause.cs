51c4b00f4ff9f1b2dabc2f12f1c1f4ccbf56ede7
Philipp-Schoenfeld <philipp.k.schoenfeld@gmail.com>
2023-10-27 18:19:18 +1000

    fix: pause bug
    multiple presses of the pause button does not result in multiple pause screens anymore.
    Continue/Menu button color adjusted

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -8,26 +8,36 @@ public class Pause : MonoBehaviour
     public GameObject anchor;
     public AnamolySpawner game;
     private GameObject pauseScreenInstance;
+    public bool gamePaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gamePaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(pauseScreenInstance == null){
+            gamePaused = false;
             game.continueGame();
+        }
+        else{
+            gamePaused = true;
         }
 
     }
     public void pauseGame()
     {
-        pauseScreenInstance = Instantiate(pauseScreen, anchor.transform);
-        pauseScreenInstance.transform.SetParent(anchor.transform);
-        game.pauseGame();
-        
+        if(!gamePaused){
+            pauseScreenInstance = Instantiate(pauseScreen, anchor.transform);
+            pauseScreenInstance.transform.SetParent(anchor.transform);
+            game.pauseGame();
+            gamePaused = true;
+        }
 
+    }
+    public void gamecontinue(){
+        gamePaused = false;
     }
 }
